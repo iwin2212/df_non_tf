a476d8f846014e56f84460bf60d122f0de40bc38
thang2212 <nbthang2212@gmail.com>
2021-05-14 14:22:37 +0700

    ha can remote with one pic per scripts

--- const.py
+++ const.py
@@ -3,8 +3,8 @@ import os
 model_path = "model/facenet.tflite"
 embedding_path = "database/embed.npy"
 
-# video_source = "rtsp://admin:ECSIAQ@192.168.1.47:554"
-video_source = 0
+video_source = "rtsp://admin:ECSIAQ@192.168.1.47:554"
+# video_source = 0
 
 snap_path = "static/snap_shots"
 UPLOAD_FOLDER = os.path.join(os.path.dirname(
--- utils.py
+++ utils.py
@@ -156,61 +156,57 @@ def predict_img_local(img_path, df):
 def predict_img_ha(list_img_path, df, face_cascade):
     candidate_label = 'unknown'
     if (str(type(list_img_path)) == "<class 'str'>"):
-        list_identity = []
-        for img in list_img_path[:3]:
-            img = cv2.imread(list_img_path)
-            while(img.shape[0] > input_shape_size):
-                img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
-            faces = face_cascade.detectMultiScale(img,  1.3, 5)
-            for (x, y, w, h) in faces:
-                if w > w_min:  # discard small detected faces
-                    # -------------------------------
-                    # apply deep learning for custom_face
-                    base_img = img.copy()
-                    img, region = functions.detect_face(
-                        img=img, enforce_detection=False)
-                    # --------------------------
+        img = cv2.imread(list_img_path)
+        while(img.shape[0] > input_shape_size):
+            img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+        faces = face_cascade.detectMultiScale(img,  1.3, 5)
+        for (x, y, w, h) in faces:
+            if w > w_min:  # discard small detected faces
+                # -------------------------------
+                # apply deep learning for custom_face
+                base_img = img.copy()
+                img, region = functions.detect_face(
+                    img=img, enforce_detection=False)
+                # --------------------------
 
-                    if img.shape[0] > 0 and img.shape[1] > 0:
-                        img = functions.align_face(img=img)
-                    else:
-                        img = base_img.copy()
-                    # --------------------------
-                    # post-processing
-                    img = cv2.resize(img, input_shape)
-                    img_pixels = np.array(img, dtype=np.float32)
-                    face_pixels = np.expand_dims(img_pixels, axis=0)
-                    face_pixels /= 255  # normalize input in [0, 1]
+                if img.shape[0] > 0 and img.shape[1] > 0:
+                    img = functions.align_face(img=img)
+                else:
+                    img = base_img.copy()
+                # --------------------------
+                # post-processing
+                img = cv2.resize(img, input_shape)
+                img_pixels = np.array(img, dtype=np.float32)
+                face_pixels = np.expand_dims(img_pixels, axis=0)
+                face_pixels /= 255  # normalize input in [0, 1]
 
-                    # print(time.time(), "face_pixels")
+                # print(time.time(), "face_pixels")
 
-                    # check preprocess_face function handled
-                    if face_pixels.shape[1:3] == input_shape:
-                        if df.shape[0] > 0:
-                            img1_representation = predict_tfmodel(face_pixels)[
-                                0, :]
+                # check preprocess_face function handled
+                if face_pixels.shape[1:3] == input_shape:
+                    if df.shape[0] > 0:
+                        img1_representation = predict_tfmodel(face_pixels)[
+                            0, :]
 
-                            def findDistance(row):
-                                img2_representation = row['embedding']
-                                distance = dst.findCosineDistance(
-                                    img1_representation, img2_representation)
-                                return distance
-                            df['distance'] = df.apply(findDistance, axis=1)
-                            # print(time.time(), "predict")
-                            df = df.sort_values(by=["distance"])
-                            # print(time.time(), "sort")
-                            # print(df)
-                            # print('--------------------')
-                            list_candidates = df.iloc[0:3]['employee'].tolist()
-                            if list_candidates.count(list_candidates[0]) >= 2:
-                                candidate_label = list_candidates[0]
-                            elif list_candidates.count(list_candidates[1]) >= 2:
-                                candidate_label = list_candidates[1]
-                            else:
-                                candidate_label = 'unknown'
-                            # print(time.time(), "get 1 in 3")
-            list_identity.append(candidate_label)
-        result = max(list_identity, key=list_identity.count)
+                        def findDistance(row):
+                            img2_representation = row['embedding']
+                            distance = dst.findCosineDistance(
+                                img1_representation, img2_representation)
+                            return distance
+                        df['distance'] = df.apply(findDistance, axis=1)
+                        # print(time.time(), "predict")
+                        df = df.sort_values(by=["distance"])
+                        # print(time.time(), "sort")
+                        # print(df)
+                        # print('--------------------')
+                        list_candidates = df.iloc[0:3]['employee'].tolist()
+                        if list_candidates.count(list_candidates[0]) >= 2:
+                            candidate_label = list_candidates[0]
+                        elif list_candidates.count(list_candidates[1]) >= 2:
+                            candidate_label = list_candidates[1]
+                        else:
+                            candidate_label = 'unknown'
+        result = candidate_label
     else:
         list_identity = []
         for img in list_img_path[:3]:
